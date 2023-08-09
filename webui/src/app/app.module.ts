e680a9aaf4863a0132baaf1b48cde4eb94c208b1
Joel Wetzell <jwetzell@yahoo.com>
2023-08-09 07:36:05 -0500

    clean out unused material modules

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -5,19 +5,15 @@ import { CdkDrag, CdkDragHandle, CdkDragPlaceholder, CdkDropList } from '@angula
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTreeModule } from '@angular/material/tree';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ActionComponent } from './components/action/action.component';
@@ -47,20 +43,16 @@ import { TriggerComponent } from './components/trigger/trigger.component';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatCardModule,
     MatIconModule,
-    MatTreeModule,
     FormsModule,
     ReactiveFormsModule,
     MatTabsModule,
-    MatSlideToggleModule,
     MatButtonModule,
     MatToolbarModule,
     MatMenuModule,
     MatInputModule,
     MatSnackBarModule,
     MatFormFieldModule,
-    MatCheckboxModule,
     CdkDropList,
     CdkDrag,
     CdkDragPlaceholder,
