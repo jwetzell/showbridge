63f553b3db04207ea36cdd5ec0cbe25bc3a19e6c
Joel Wetzell <jwetzell@yahoo.com>
2023-08-31 09:26:20 -0500

    update tests

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ const schema = require('../schema/config.schema.json');
 import('../lib/index.js').then(({ Config, Router, Utils, Messages }) => {
   const logger = Utils.logger;
 
-  const { MIDIMessage, UDPMessage, TCPMessage, MQTTMessage, WebSocketMessage, HTTPMessage } = Messages;
+  const { OSCMessage, MIDIMessage, UDPMessage, TCPMessage, MQTTMessage, WebSocketMessage, HTTPMessage } = Messages;
 
   logger.info(`app: loading test config`);
   const config = new Config(testConfig, schema);
@@ -16,27 +16,36 @@ import('../lib/index.js').then(({ Config, Router, Utils, Messages }) => {
     // MIDI
     new MIDIMessage([0x80, 60, 100], 'test'),
     new MIDIMessage([0x90, 60, 100], 'test'),
-    // new MIDIMessage([0xa0, 60, 100], 'test'),
+    new MIDIMessage([0xa0, 60, 100], 'test'),
     new MIDIMessage([0xb0, 60, 100], 'test'),
     new MIDIMessage([0xc0, 60, 100], 'test'),
-    // new MIDIMessage([0xd0, 60, 100], 'test'),
-    // new MIDIMessage([0xe0, 60, 100], 'test'),
+    new MIDIMessage([0xd0, 60, 100], 'test'),
+    new MIDIMessage([0xe0, 60, 100], 'test'),
 
     // UDP
-    new UDPMessage('string', { address: '127.0.0.1', port: 0 }),
-    new UDPMessage(Buffer.from('buffer'), { address: '127.0.0.1', port: 0 }),
+    new UDPMessage('string udp message', { address: '127.0.0.1', port: 0 }),
+    new UDPMessage(Buffer.from('buffer udp message'), { address: '127.0.0.1', port: 0 }),
 
     // TCP
-    new TCPMessage('string', { address: '127.0.0.1', port: 0 }),
-    new TCPMessage(Buffer.from('buffer'), { address: '127.0.0.1', port: 0 }),
+    new TCPMessage('string tcp message', { address: '127.0.0.1', port: 0 }),
+    new TCPMessage(Buffer.from('buffer tcp message'), { address: '127.0.0.1', port: 0 }),
+
+    // OSC
+    new OSCMessage(
+      {
+        address: '/test',
+        args: [],
+      },
+      { address: '127.0.0.1', port: 0 }
+    ),
 
     // MQTT
-    new MQTTMessage('hello', 'test'),
-    new MQTTMessage(JSON.stringify({ hello: 'world' }), 'test'),
+    new MQTTMessage('simple string mqtt message', 'test'),
+    new MQTTMessage(JSON.stringify({ data: 'object mqtt message' }), 'test'),
 
     // WebSocket
-    new WebSocketMessage('hello', 'test'),
-    new WebSocketMessage(JSON.stringify({ hello: 'world' }), 'test'),
+    new WebSocketMessage('simple string websocket message', 'test'),
+    new WebSocketMessage(JSON.stringify({ data: 'object websocket message' }), 'test'),
 
     // HTTP
     new HTTPMessage({
