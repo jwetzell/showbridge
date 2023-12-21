296515746d9344f4db49132208bfa59efa75bfdf
Joel Wetzell <jwetzell@yahoo.com>
2023-12-20 23:22:15 -0600

    eslint ignore rule

--- launcher/electron_bundle.js
+++ launcher/electron_bundle.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 
+// eslint-disable-next-line no-unused-vars
 exports.default = async function (context) {
   console.log('bundling things up for electron app');
 
