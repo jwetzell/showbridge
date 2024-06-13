248151c343c74e9bb79aaaa2e5ee40b0aae6c205
Joel Wetzell <jwetzell@yahoo.com>
2024-06-12 21:32:59 -0500

    add missing check formt lint script

---
+++ scripts/check_format_lint.js
@@ -0,0 +1,24 @@
+import cp from 'node:child_process';
+
+const eslintPaths = ['./cli', './cloud', './launcher', './lib', './site', './webui'];
+
+const pathsWithErrors = [];
+
+console.log(`Running eslint on:\n${eslintPaths.join('\n')}`);
+eslintPaths.forEach((path) => {
+  const eslintProcess = cp.spawnSync('eslint', [path], {
+    stdio: 'inherit',
+  });
+
+  if (eslintProcess.error || eslintProcess.status > 0) {
+    console.log(eslintProcess.error);
+    pathsWithErrors.push(path);
+  }
+});
+
+if (pathsWithErrors.length > 0) {
+  console.error(`The following paths have formatting or linting errors: ${pathsWithErrors.join(', ')}`);
+  process.exit(1);
+} else {
+  console.log('No formatting or linting errors found.');
+}
